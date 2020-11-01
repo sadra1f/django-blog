7e6767d69295eca0befc6e8bc6aae6c6184a14c9
Sadra Saderi <sadrassx@gmail.com>
2020-11-01 22:19:32 +0330

    Add blog and post pages

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from blog import views as v
 
 urlpatterns = [
-    path('', v.home, name='home')
+    path('', v.home, name='home'),
+    path('posts/', v.blog, name='blog'),
+    path('post/', v.post, name='post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'blog/index.html')
+
+def blog(request):
+    return render(request, 'blog/blog.html')
+
+def post(request):
+    return render(request, 'blog/post.html')
